272c79ecb2e0f44e3ad088fd859664512537c4ab
oscarcast22 <oscar_cm96@outlook.com>
2024-10-30 13:53:21 -0600

    first commit

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,31 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `npm run dev` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `npm run deploy` to publish your worker
- *
- * Bind resources to your worker in `wrangler.toml`. After adding bindings, a type definition for the
- * `Env` object can be regenerated with `npm run cf-typegen`.
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { Hono } from 'hono';
+import { cors } from 'hono/cors'
+import { Ai } from '@cloudflare/ai';
 
-export default {
-	async fetch(request, env, ctx): Promise<Response> {
-		return new Response('Hello World!');
-	},
-} satisfies ExportedHandler<Env>;
+type Env = {
+	AI: Ai;
+  };
+
+const app = new Hono<{ Bindings: Env }>();
+app.use(cors());
+
+app.get('/query', async (c) => {
+	const question = c.req.query('text');
+	if (!question || question.trim().length < 1) return c.text('Escribe una solicitud valida')
+
+	const systemPrompt = 'Eres un asistente de conversación que responde a las preguntas de los usuarios en un estilo natural. Responde solo en Español, evita responder dos veces en diferente idioma, solo se requiere el texto en español'
+
+	const response = await c.env.AI.run(
+	  	"@cf/mistral/mistral-7b-instruct-v0.2-lora",
+	  	{
+			messages: [
+				{ role: 'system', content: systemPrompt },
+		  		{ role: 'user', content: question }
+			] as RoleScopedChatInput[]
+	  	}
+	) as AiTextGenerationOutput
+
+	return response ? c.text((response as any).response) : c.text("Hubo un error generando la respuesta, por favor vuelve a interarlo", 500)
+})
+
+export default app;
